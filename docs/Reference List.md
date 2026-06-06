35a63ba622033a47eb5da40bf6a36a66d38f7341
V4LKdev <v4lk@duck.com>
2026-06-06 16:25:03 +0200

    Added simple integer-based fixed-timestep simulation loop

---
+++ docs/Reference List.md
@@ -0,0 +1,9 @@
+
+### Used for fixed TPS simulation loop:
+https://www.gafferongames.com/post/fix_your_timestep/
+Relevancy: 4/5 
+Very relevant to my work, but not concretely the topic of my research
+
+https://ahmadsarraj.com/075-fixed-time-steps-for-deterministic-physics-simulation/
+Relevancy 4/5
+Same reasons, this one concretely talks about Spiral of death and the float precision and integer time
--- src/client/src/main.cpp
+++ src/client/src/main.cpp
@@ -1,20 +1,50 @@
+#include <chrono>
 #include <iostream>
 #include <enet/enet.h>
 #include <flecs.h>
 #include <raylib.h>
 
-int main() {
+void simulation() {
 
-    InitWindow(800, 450, "Thesis Client Dev Window");
-    SetTargetFPS(60);
+}
+
+constexpr int SIM_HZ = 30;
+constexpr std::chrono::nanoseconds SIM_DT = std::chrono::nanoseconds(1'000'000'000 / SIM_HZ);
+constexpr int MAX_SIM_STEPS = 5;
+constexpr auto MAX_ACCUM_NS = SIM_DT.count() * MAX_SIM_STEPS; // Spiral-of-death capping
+
+int main() {
+    using Clock = std::chrono::steady_clock;
+
+    uint64_t sim_time_ns = 0;
+    int64_t accumulator_ns = 0;
+    std::chrono::time_point<Clock> last_time = Clock::now();
+
+    InitWindow(800, 450, "SimNetLab_Client");
+    SetTargetFPS(0);
 
     while (!WindowShouldClose()) {
+
+        std::chrono::time_point<Clock> now = Clock::now();
+        std::chrono::nanoseconds frame_delta =
+            std::chrono::duration_cast<std::chrono::nanoseconds>(now - last_time);
+        last_time = now;
+
+        // accumulate time but cap for spiral prevention
+        accumulator_ns += frame_delta.count();
+        accumulator_ns = std::min(accumulator_ns, MAX_ACCUM_NS);
+
+        // Simulation Loop
+        while (accumulator_ns >= SIM_DT.count())
+        {
+            sim_time_ns += SIM_DT.count();
+            accumulator_ns -= SIM_DT.count();
+
+            simulation();
+        }
+
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-
-        DrawText("Hello, World!", 190, 200, 20, DARKGRAY);
-        DrawRectangle(10, 10, 50, 50, RED);
-
+        ClearBackground(DARKGRAY);
         EndDrawing();
     }
 
